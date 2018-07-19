f7f1fc51cd7fefc89946b1526bcdacc48e1c00c7
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-07-19 16:47:58 -0700

    Made changes based on implementing ESLint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../client'))
+app.use(express.static('../client'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
