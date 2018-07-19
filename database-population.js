906009bad79fe95d3890ef49f757d68865604dbf
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-07-18 20:12:13 -0700

    Completed data generation script and loaded mock data into mongo.

--- database-population.js
+++ database-population.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const db = require('./database/reviews');
 
 const numUsers = 100;
 const numReviews = 500;
@@ -7,12 +8,16 @@ const arrayOfAvatarURLs = ['https://s3-us-west-1.amazonaws.com/fec-reviews/1.jpg
 
 let arrayOfUsers = [];
 
-// do users need a unique id
+const generateRandomNumber = function (val) {
+    return Math.ceil(val * Math.random());
+}
+
 const userGeneration = function () {
     for (let i = 0; i < numUsers; i++) {
         let tempObject = {};
+        tempObject.userId = i;
         tempObject.name = faker.name.findName();
-        tempObject.averageRating = 5 * Math.random();
+        tempObject.avgRating = generateRandomNumber(5);
         // will break in the case where Math.random() returns 1 - not sure if - 0.01 will work
         tempObject.picture = arrayOfAvatarURLs[Math.floor((arrayOfAvatarURLs.length - 0.01) * Math.random())];
         arrayOfUsers.push(tempObject);
@@ -24,15 +29,44 @@ let arrayOfReviews = [];
 const reviewGeneration = function () {
     for (let i = 0; i < numReviews; i++) {
         let tempObject = {};
-        tempObject.houseId = Math.floor(numReviews * Math.random());
+        tempObject.houseId = generateRandomNumber(numUsers);
         tempObject.reviewTitle = faker.lorem.sentence();
         tempObject.reviewText = faker.lorem.paragraph();
         tempObject.reviewDate = faker.date.past();
-        tempObject.helpfulCount = Math.floor(10 * Math.random());
-        tempObject.reportedCount = {inappropriate: Math.floor(10 * Math.random()), hateful: Math.floor(10 * Math.random()), fake: Math.floor(10 * Math.random())}
-        tempObj.response = {name: `${faker.name.firstName()} ${faker.name.lastName}`, responseText: faker.lorem.sentences(), responseDate: faker.date.past(), responsePicture: arrayOfAvatarURLs[Math.floor((arrayOfAvatarURLs.length - 0.01) * Math.random())]};
-        tempObj.rating = {overall: Math.floor(5 * Math.random()), interior: Math.floor(5 * Math.random()), exterior: Math.floor(5 * Math.random()), location: Math.floor(5 * Math.random()), noise: Math.floor(5 * Math.random())};
-        tempObj.group = {solo: Math.floor(2 * Math.random()), couple: Math.floor(2 * Math.random()), friends: Math.floor(2 * Math.random()), family: Math.floor(2 * Math.random())};
+        tempObject.helpfulCount = generateRandomNumber(10);
+        tempObject.reportedCount = {inappropriate: generateRandomNumber(10), hateful: generateRandomNumber(10), fake: generateRandomNumber(10)}
+        tempObject.response = {name: faker.name.findName(), responseText: faker.lorem.sentences(), responseDate: faker.date.past(), responsePicture: arrayOfAvatarURLs[Math.floor((arrayOfAvatarURLs.length - 0.01) * Math.random())]};
+        tempObject.rating = {overall: generateRandomNumber(5), interior: generateRandomNumber(5), exterior: generateRandomNumber(5), location: generateRandomNumber(5), noise: generateRandomNumber(5)};
+        // 1 is solo, 2 is group, 3 is couple, 4 is family
+        tempObject.group = generateRandomNumber(4);
+        tempObject.user = Math.floor(99 * Math.random());
         arrayOfReviews.push(tempObject);
     }
 }
+
+userGeneration();
+
+console.log('Array of Users: ', arrayOfUsers);
+
+const populateUsers = function () {
+    db.User.insertMany(arrayOfUsers, function (error, docs) {
+        if (error) console.log(error);
+        console.log(docs);
+    });
+}
+
+populateUsers();
+
+reviewGeneration();
+
+const populateReviews = function () {
+    db.Review.insertMany(arrayOfReviews, function (error, docs) {
+        if (error) console.log(error);
+        console.log(docs);
+    });
+}
+
+populateReviews();
+
+//tell node to terminate 
+// process.exit(-1);
--- database/reviews.js
+++ database/reviews.js
@@ -1,25 +1,35 @@
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fec');
+
 const Schema = mongoose.Schema;
 
-// does mongoose allow trailing commas
 const userSchema = new Schema ({
+    userId: Number,
     name: String,
-    // need to fix this to take a url from Amazon s3
     picture: String,
     avgRating: Number,
 })
 
 const reviewSchema = new Schema ({
-    houseId = Number,
-    reviewTitle = String,
-    reviewText = String,
-    reviewDate = Date,
-    helpfulCount = Number,
-    reportedCount = {inappropriate: Number, hateful: Number, fake: Number},
+    houseId: Number,
+    reviewTitle: String,
+    reviewText: String,
+    reviewDate: Date,
+    helpfulCount: Number,
+    reportedCount: {inappropriate: Number, hateful: Number, fake: Number},
     // in a real backend this would reference user to get name and responsePicture
-    response = {name: String, responseText: String, responseDate: Date, responsePicture},
-    rating = {overall: Number, interior: Number, exterior: Number, location: Number, noise: Number},
-    group: {solo: Boolean, couple: Boolean, friends: Boolean, family: Boolean},
-    // need to fix to link to user table
-    user: {},
+    response: {name: String, responseText: String, responseDate: Date, responsePicture: String},
+    rating: {overall: Number, interior: Number, exterior: Number, location: Number, noise: Number},
+    group: Number,
+    // in the real world I would use the _id property on each document
+    user: Number,
 })
+
+const User = mongoose.model('User', userSchema);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = {
+    User,
+    Review,
+}
