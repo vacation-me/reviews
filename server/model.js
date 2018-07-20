e57f05577b8c3dfa118290202f265083ab7f9668
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-07-19 22:45:04 -0700

    Setup get reviews route to return user object and review object

--- server/model.js
+++ server/model.js
@@ -1,14 +1,41 @@
 const db = require('../database/index');
+const _ = require('underscore');
 
 module.exports = {
   reviews: {
     getReviews: function (cb) {
-      db.Review.find({}, null, function (err, reviews) {
-        if (err) {
-          console.log('error in model');
+      // const reviews = [];
+      // const combinedUsers = [];
+      const promises = [];
+      let combinedReview = [];
+      db.Review.find({}, null).then((reviews) => {
+        for (let review of reviews) {
+          // console.log(combinedUsers); 
+          // push something to promises
+          // console.log(review);
+          promises.push(db.User.findOne({userId: review.user}, null, function (err, user) {
+            if (err) {
+              return err;
+            }
+            // $.extend({}, object1, object2);
+            console.log(review);
+            // console.log(user);
+            // console.log(_.extend(review, user));
+            let combinedObject = [review, user];
+            // let combinedObject = _.extend(review, user);
+            // console.log(combinedObject);
+            combinedReview.push(combinedObject); 
+            // console.log('internal', combinedUsers);
+          }));
         }
-        cb(reviews);
+        // console.log(combinedUsers);
+      }).then(() => {
+        Promise.all(promises).then(() => {
+          cb(combinedReview);
+        });
       });
+
+      // call Promise.all(promises).then()
     }
   }
 };
