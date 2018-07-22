895085d970caab22e14817f9436cc563523b2abd
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-07-22 12:10:51 -0700

    Added css grid and tranformed date received

--- client/components/ReviewEntry.jsx
+++ client/components/ReviewEntry.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import style from './styles/EntryStyle.css';
 
 const ReviewEntry = (props) => {
   console.log(props);
 
+  const formatDate = function (date) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let newDate;
+    newDate = date.slice(0, date.indexOf('T'));
+    newDate = newDate.split('-');
+    // console.log(newDate);
+    let month = months[newDate[1] - 1];
+    // console.log(month);
+    let finalString = `${month} ${newDate[0]}`;
+    return finalString;
+  };
+
   return (
-    <div>
-      <img src={props.rev[1].picture} />
-      <div>{props.rev[1].name}</div>
-      <div>{props.rev[0].reviewDate}</div>
-      <div>{props.rev[0].reviewText}</div>
+    <div id="entryContainer">
+      <img src={props.rev[1].picture} id="reviewAvatar" />
+      <div id="reviewName">{props.rev[1].name}</div>
+      <div id="reviewDate">{formatDate(props.rev[0].reviewDate)}</div>
+      <div id="reviewText">{props.rev[0].reviewText}</div>
     </div>
   );
 };
