72ecd45b22264cdc54083bd193ce3cf2c6b3aa67
DavidPatterson-Cole <d.patterson-cole@queensu.ca>
2018-07-27 12:53:01 -0700

    Added test to check that 5 stars render

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import App from '../client/components/App.jsx';
+import AggregatedReviews from '../client/components/AggregatedReviews';
 
-describe('Test rendering of reviews heading', () => {
-  test('title equals Review', () => {
-    const wrapper = shallow(<App />);
-    const text = wrapper.find('#reviewList').text();
-    expect(text).toEqual('Reviews');
+const ratings = {
+  accuracy: 2.6666666666666665,
+  checkIn: 3,
+  cleanliness: 3.5, 
+  communication: 3.1666666666666665,
+  location: 3,
+  overall: 4.333333333333333,
+  value: 3.1666666666666665
+};
+
+describe('Check AggregatedReviewComponent', () => {
+  test('Each all 6 review categories to have five star icons each', () => {
+    const wrapper = mount(<AggregatedReviews ratings={ratings} />);
+    expect(wrapper.find('.starSet').children().length).toEqual(30);
   });
 });
