add121b844a408f4e02663d070598aebed3e2abd
Michael Lee <lee.cs.michael@gmail.com>
2018-08-17 12:01:57 -0700

    Refactor front-end code to verify UI still works as expected

--- client/components/ReviewEntry.jsx
+++ client/components/ReviewEntry.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './styles/EntryStyle.css';
 
 const ReviewEntry = (props) => {
-  // console.log(props);
 
   const formatDate = function (date) {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -18,10 +17,10 @@ const ReviewEntry = (props) => {
 
   return (
     <div id={styles.entryContainer}>
-      <img src={props.rev[1].picture} id={styles.reviewAvatar} />
-      <div id={styles.reviewName}>{props.rev[1].name}</div>
-      <div id={styles.reviewDate}>{formatDate(props.rev[0].reviewDate)}</div>
-      <div id={styles.reviewText}>{props.rev[0].reviewText}</div>
+      <img src={props.rev.picture} id={styles.reviewAvatar} />
+      <div id={styles.reviewName}>{props.rev.name}</div>
+      <div id={styles.reviewDate}>{props.rev.reviewdate}</div>
+      <div id={styles.reviewText}>{props.rev.reviewtext}</div>
       <div id={styles.reviewFlag}><img id={styles.flagIcon} src='https://s3-us-west-1.amazonaws.com/fec-reviews/flag.svg' /></div>
       <div id={styles.reviewBorder}></div>
     </div>
